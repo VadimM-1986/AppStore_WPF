7cad82023e41cfc39f5c443e3c45d815081d3416
Vadim <maryushkin.vadim@gmail.com>
2023-12-27 13:32:50 +0300

    first commit

---
+++ Diplomka/AppDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppStore.Models;
+
+
+namespace AppStore
+{
+    internal class AppDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public AppDBContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql("Server=localHost; Database=diplomka; Port=3306; User=root; Password=root");
+        }
+    }
+}
