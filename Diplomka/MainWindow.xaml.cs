2bd50e6de4cf1732e936bdb830cde5185f0ac89d
Vadim <maryushkin.vadim@gmail.com>
2023-12-27 13:29:24 +0300

    First commit

---
+++ Diplomka/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using AppStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using CloudIpspSDK;
+using CloudIpspSDK.Checkout;
+using System.Collections.Generic;
+using System.Data;
+
+namespace AppStore
+{
+
+    public partial class MainWindow : Window
+    {
+        public List<int> IdProd = new List<int>();
+        public int summa;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadProducts();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void LoadProducts()
+        {
+            using (AppDBContext context = new AppDBContext())
+            {
+                var products = context.Products.ToList();
+                productListBox.ItemsSource = products;
+
+            }
+        }
+
+        private void buttonAddCart_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            int tagValue = Convert.ToInt32(button.Tag);
+
+            IdProd.Add(tagValue);
+
+            zagcart.Content = "В корзине есть товары";
+ 
+            int countKolvo = 0;
+            foreach (int prod in IdProd)
+            {
+                countKolvo++;
+            }
+            kolvoText.Content = $"Количество: {countKolvo} шт.";
+
+
+            using (AppDBContext context = new AppDBContext())
+            {
+                var stoimost = context.Products.Where(el => el.Id == tagValue);
+
+                foreach (var prod in stoimost)
+                {
+                    summa += prod.Price;
+                }
+            }
+            StoimostText.Content = $"Общая стоимость: {summa}$";
+        }
+
+        private void buttonCart_Click(object sender, RoutedEventArgs e)
+        {
+            if (IdProd.Count <= 0)
+            {
+                MessageBox.Show("Ваша корзина пуста!");
+            }
+            else
+            {
+                Config.MerchantId = 1396424;
+                Config.SecretKey = "test";
+
+                var req = new CheckoutRequest
+                {
+                    order_id = Guid.NewGuid().ToString("N"),
+                    amount = summa * 100,
+                    order_desc = "checkout json demo",
+                    currency = "USD"
+                };
+                var resp = new Url().Post(req);
+
+                if (resp.Error != null)
+                {
+                    MessageBox.Show("Произошла какая то ошибка!");
+                }
+                else
+                {
+                    string url = resp.checkout_url;
+                    System.Diagnostics.Process.Start(url);
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ Diplomka/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, string description, string img, int price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Img = img;
+            this.Price = price;
+        }
+
+
+        public string GetFoto
+        {
+            get { return "/images/tovari/" + Img; }
+        }
+
+    }
+}
